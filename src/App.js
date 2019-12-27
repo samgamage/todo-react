b1f668743a0b380abd057c4b9d7bdbc7ff55d4a4
samgamage <sammygamage@gmail.com>
2019-12-27 14:51:22 -0500

    Define todos state and form UI

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
-import logo from "./logo.svg";
+import TodoForm from "./components/TodoForm";
 
 function App() {
+  const [todos, setTodos] = useState([]);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Todo app</p>
+        <TodoForm />
       </header>
     </div>
   );
---
+++ src/components/TodoForm.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function TodoForm() {
+  return (
+    <form>
+      <input type="text" />
+      <button type="submit">Submit</button>
+    </form>
+  );
+}
+
+export default TodoForm;
